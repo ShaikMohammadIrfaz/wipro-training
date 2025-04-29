15568229b9e57b86de86f481e6d345afe8f21616
Mohammad Irfaz <2000031732cse@gmail.com>
2025-04-29 17:00:51 +0530

    Assignment 3

---
+++ 29-April/Code/3rd.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number = 5;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
