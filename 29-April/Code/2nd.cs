dcdd0aa91486406dad64df64bb63704d0bdc3bf5
Mohammad Irfaz <2000031732cse@gmail.com>
2025-04-29 16:18:27 +0530

    2nd code

---
+++ 29-April/Code/2nd.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number = 7; 
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine(number + " is Even.");
+        }
+        else
+        {
+            Console.WriteLine(number + " is Odd.");
+        }
+    }
+}
